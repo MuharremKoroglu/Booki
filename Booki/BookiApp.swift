65df846b9e1779c1288c77aa60b2f90a4eb6da8f
Muharrem Koroglu <muharrem-koroglu.99@hotmail.com>
2022-12-27 14:07:34 +0300

    Initial Commit

---
+++ Booki/BookiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookiApp.swift
+//  Booki
+//
+//  Created by Muharrem Köroğlu on 27.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct BookiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
